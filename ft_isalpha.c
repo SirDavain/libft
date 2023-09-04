ec3648020f8851a8d1abc1895e3dd56024d87982
David Ulrich <dulrich@c4r4p6.42vienna.com>
2023-09-04 14:02:53 +0200

    isalpha, isdigit and isalnum are done

---
+++ ft_isalpha.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 12:44:44 by dulrich           #+#    #+#             */
+/*   Updated: 2023/09/04 13:49:54 by dulrich          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_isupper(int c)
+{
+    if (c >= 65 && c <= 90)
+        return (1);
+}
+
+int ft_islower(int c)
+{
+    if (c >= 97 && c <= 122)
+        return (1);
+}
+
+int ft_isalpha(int c)
+{
+    if (ft_isupper || ft_islower)
+        return (1);
+    else
+        return (0);
+}
+
+/*#include <stdio.h>
+int main(void)
+{
+    int c;
+
+    c = 'h';
+    printf("%i", ft_isalpha(c));
+}*/
