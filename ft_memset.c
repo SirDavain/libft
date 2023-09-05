b72c2620041216a8c56c46f47f725f56bfa22702
David Ulrich <dulrich@c4r4p5.42vienna.com>
2023-09-05 17:07:53 +0200

    updating and going home

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:06:57 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/05 15:12:06 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/05 17:07:39 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 void	ft_memset(void *s, int c, size_t n)
 {
-	while (n && (void *)s)
+	int	i;
+
+	i = 0;
+	while (n > i)
 	{
-		(char *)s = c;
-		s++;
-		n--;
+		s = c;
+		i++;
 	}
-	return ((void *)s);
 }
 
 int	main(void)
