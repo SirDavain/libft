cd5f5d402af549dfc8a9132795a2e01430e69256
David Ulrich <dulrich@c4r4p5.42vienna.com>
2023-09-06 14:16:33 +0200

    memmove should be close to working correctly

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 12:01:58 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/06 12:29:54 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/06 14:15:45 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,39 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	tmp_arr[100];
 	unsigned char	*un_dest;
 	unsigned char	*un_src;
 	int				i;
 
 	un_dest = (unsigned char *)dest;
 	un_src = (unsigned char *)src;
-	i = 0;
-	
-	/*two while loops: one for if it is not overlapping --> normally copy
-	the other one for if it is overlapping -> calc length of the src and copy backwards
-	with length--*/
-	while (un_src[i] && n > i)
+	if (un_dest > un_src && un_src + n > un_dest)
 	{
-		tmp_arr[i] = un_src[i];
-		un_dest[i] = tmp_arr[i];
-		i++;
+		i = n;
+		while (i)
+		{
+			*(un_dest + i) = *(un_src + i);
+			i--;
+		}
+		return ((void *)un_dest);
 	}
-	return (dest);
+	else
+	{
+		i = 0;
+		while (n > i)
+		{
+			*(un_dest + i) = *(un_src + i);
+			i++;
+		}
+		return ((void *)un_dest);
+	}
 }
 
 #include <stdio.h>
 int main(void)
 {
 	unsigned char dest[50];
-	unsigned char src[11] = "Hello World";
+	unsigned char src[] = "Hello World";
 	size_t n = 5;
 
 	*ft_memmove(dest, src, n);
