5d52d81d3b2754187f025caf1a7ca278ec9f4d9a
David Ulrich <dulrich@c4r4p5.42vienna.com>
2023-09-08 08:56:48 +0200

    strlcat should be finished

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:30:01 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/05 14:48:55 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/08 08:56:24 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 size_t	strlcat(char *dst, const char *src, size_t size)
 {
-	int	i;
-	int	j;
-	int	dst_len;
-	int	src_len;
+	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
 	i = 0;
-	while (dst[i])
+	while (dst[i] && i < size)
 		i++;
 	j = 0;
-	while (src[j] && size - ft_strlen(dst) - 1)
+	while (src[j] && i + 1 < size)
 	{
-		dst[i++] = src[j++];
-		size--;
+		dst[i] = src[j];
+		i++;
+		j++;
 	}
-	dst[i] = '\0';
+	if (i < size)
+		dst[i] = '\0';
 	return (dst_len + src_len);
 }
