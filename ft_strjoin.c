84e5606b23e7cb083602243cf37014fd61984453
David Ulrich <dulrich@c4r4p5.42vienna.com>
2023-09-08 10:11:09 +0200

    strjoin is working

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,46 +6,64 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 12:38:33 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/07 13:16:37 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/08 10:09:47 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+static size_t	ft_totallen(const char *s1, const char *s2)
 {
 	size_t	i;
+	size_t	j;
 
 	i = 0;
-	while (s[i])
+	while (s1[i])
 		i++;
+	j = 0;
+	while (s2[j])
+	{
+		i++;
+		j++;
+	}
 	return (i);
 }
 
-char	ft_strcpy(char *s)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
-}
-
-char *ft_strjoin(char const *s1, char const *s2)
-{
-	int		s1_len;
-	int		s2_len;
 	int		total_len;
 	char	*s3;
 	size_t	i;
+	size_t	j;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	total_len = (s1_len + s2_len) + 1;
+	total_len = ft_totallen(s1, s2) + 1;
 	s3 = (char *)malloc((total_len) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
 	i = 0;
-	while (i < total_len)
+	while (s1[i])
 	{
-		s3[i] = 
+		s3[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
 	}
 	s3[i] = '\0';
 	return (s3);
 }
+
+/*#include <stdio.h>
+
+int main(void)
+{
+	const char	*s1 = "Hello ";
+	const char	*s2 = "World.";
+	char		*result = ft_strjoin(s1, s2);
+
+	printf("%s", result);
+}*/
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 15:38:36 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/07 13:49:05 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/08 09:30:54 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	int			i;
-	int			j;
-	const char	*big2;
-	size_t		size;
+	size_t		i;
+	size_t		j;
 
+	if (!(*little))
+		return ((char *)big);
 	i = 0;
-	if (little[i] == '\0')
-		return (big);
 	while (big[i] && i < len)
 	{
-		if (big[i] == little[i])
+		j = 0;
+		while (big[i + j] == little[j] && (i + j) < len)
 		{
-			big2 = &big[i];
-			size = len;
-			j = 0;
-			while (big2[j] == little[j++])
-			{
-				size--;
-				if (little[j] == '\0' || size == 0)
-					return (big[i]);
-			}
+			j++;
+			if (little[j] == '\0')
+				return ((char *)&big[i]);
 		}
 		i++;
 	}
-	return (NULL);
+	return ((char *)NULL);
 }
 
 #include <stdio.h>
@@ -46,6 +39,6 @@ int	main(void)
 {
 	const char	*big = "Dinosaur";
 	const char	*little = "no";
-	size_t		len = 5;
+	size_t		len = 1;
 	printf("%s", ft_strnstr(big, little, len));
 }
