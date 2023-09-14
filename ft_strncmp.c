af947e148ca23b63f2e214778b311823b874d5fb
David Ulrich <dulrich@c3r7p2.42vienna.com>
2023-09-14 17:17:54 +0200

    cast the const to unsigned chars

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 12:22:44 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/14 14:56:10 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/14 17:17:31 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*un_s1;
+	unsigned char	*un_s2;
 
+	un_s1 = (unsigned char *)s1;
+	un_s2 = (unsigned char *)s2;
 	i = 0;
-	while (*s1 && *s2)
+	while (*un_s1 && *un_s2)
 	{
-		while (s1[i] == s2[i] && n > i)
+		while (un_s1[i] == un_s2[i] && n > i)
 			i++;
-		if (s1[i] > s2[i])
+		if (un_s1[i] > un_s2[i])
 			return (1);
-		else if (s1[i] < s2[i])
+		else if (un_s1[i] < un_s2[i])
 			return (-1);
 		else
 			return (0);
 	}
+	return ;
 }
