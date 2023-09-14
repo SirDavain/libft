5f68b06f8d8cf643655871b9cb36fe1b01c22fc8
David Ulrich <dulrich@c3r7p2.42vienna.com>
2023-09-14 11:02:00 +0200

    Makefile is finished

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 10:44:13 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/13 10:27:07 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/14 10:32:23 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int	ft_toupper(int c);
-int	tolower(int c);
+int	ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
