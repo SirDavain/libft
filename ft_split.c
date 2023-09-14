357bba1d07ca6ceb475c087ffdcf138ddc24f2ab
David Ulrich <dulrich@c3r7p2.42vienna.com>
2023-09-14 13:49:25 +0200

    split is shortened and norminette is ok

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,11 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 12:26:31 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/14 10:06:17 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/14 13:48:43 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 static int	ft_count(const char *s, int c)
 {
@@ -60,28 +50,20 @@ static void	ft_free_split(char **s)
 	free(s);
 }
 
-static char	*ft_substr(char const *s, unsigned int start, size_t len)
+static char	*ft_get_next_substr(const char **s, char c)
 {
-	char	*substr;
-	size_t	i;
+	const char	*start;
+	const char	*end;
 
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		len = 0;
-	else if (start + len > ft_strlen(s))
-		len = ft_strlen(s) - start;
-	substr = (char *)malloc((len + 1) * sizeof(char));
-	if (!substr)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	substr[i] = '\0';
-	return (substr);
+	start = *s;
+	end = start;
+	while (*end && *end != c)
+		end++;
+	if (*end)
+		*s = end + 1;
+	else
+		*s = end;
+	return (ft_substr(start, 0, end - start));
 }
 
 char	**ft_split(char const *s, char c)
@@ -89,42 +71,6 @@ char	**ft_split(char const *s, char c)
 	char	**str_arr;
 	int		i;
 	int		num_of_str;
-	int		substr_len;
-
-	if (!s)
-		return (NULL);
-	num_of_str = ft_count(s, c);
-	str_arr = (char **)malloc((num_of_str + 1) * sizeof(char *));
-	if (!str_arr)
-		return (NULL);
-	i = 0;
-	while (*s)
-	{
-		while (*s && *s == c)
-			s++;
-		substr_len = 0;
-		while (s[substr_len] && s[substr_len] != c)
-			substr_len++;
-		str_arr[i] = ft_substr(s, 0, substr_len);
-		if (!str_arr[i])
-		{
-			ft_free_split(str_arr);
-			return (NULL);
-		}
-		s += substr_len;
-		i++;
-		num_of_str--;
-	}
-	str_arr[i] = NULL;
-	return (str_arr);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**str_arr;
-	int		i;
-	int		num_of_str;
-	int		substr_len;
 
 	if (!s)
 		return (NULL);
@@ -135,18 +81,12 @@ char	**ft_split(char const *s, char c)
 	i = 0;
 	while (num_of_str > 0)
 	{
-		while (*s && *s == c)
-			s++;
-		substr_len = 0;
-		while (s[substr_len] && s[substr_len] != c)
-			substr_len++;
-		str_arr[i] = ft_substr(s, 0, substr_len);
+		str_arr[i] = ft_get_next_substr(&s, c);
 		if (!str_arr[i])
 		{
 			ft_free_split(str_arr);
 			return (NULL);
 		}
-		s += substr_len;
 		i++;
 		num_of_str--;
 	}
@@ -154,20 +94,55 @@ char	**ft_split(char const *s, char c)
 	return (str_arr);
 }
 
-#include <stdio.h>
-int main(void)
-{
-	char c = ',';
-	char *s = ",,Hippo,,,,po,ta,mus,,";
-	char **result;
-	
-	result = ft_split(s, c);
-	int i = 0;
-	while (result[i])
-	{
-		printf("%s", result[i]);
-		i++;
-	}
-	ft_free_split(result);
-	return (0);
-}
+// char	**ft_split(char const *s, char c)
+// {
+// 	char	**str_arr;
+// 	int		i;
+// 	int		num_of_str;
+// 	int		substr_len;
+
+// 	if (!s)
+// 		return (NULL);
+// 	num_of_str = ft_count(s, c);
+// 	str_arr = (char **)malloc((num_of_str + 1) * sizeof(char *));
+// 	if (!str_arr)
+// 		return (NULL);
+// 	i = 0;
+// 	while (num_of_str > 0)
+// 	{
+// 		while (*s && *s == c)
+// 			s++;
+// 		substr_len = 0;
+// 		while (s[substr_len] && s[substr_len] != c)
+// 			substr_len++;
+// 		str_arr[i] = ft_substr(s, 0, substr_len);
+// 		if (!str_arr[i])
+// 		{
+// 			ft_free_split(str_arr);
+// 			return (NULL);
+// 		}
+// 		s += substr_len;
+// 		i++;
+// 		num_of_str--;
+// 	}
+// 	str_arr[i] = NULL;
+// 	return (str_arr);
+// }
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char c = ',';
+// 	char *s = ",,Hippo,,,,po,ta,mus,,";
+// 	char **result;
+
+// 	result = ft_split(s, c);
+// 	int i = 0;
+// 	while (result[i])
+// 	{
+// 		printf("%s", result[i]);
+// 		i++;
+// 	}
+// 	ft_free_split(result);
+// 	return (0);
+// }
--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 15:23:44 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/13 16:43:13 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/14 13:47:27 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
-	char			tmp;
 
 	if (!s || !f)
 		return ;
