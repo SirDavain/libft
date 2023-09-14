cc9852f2a33bf12af1c1f419373f6dd9097f1171
David Ulrich <dulrich@c3r7p2.42vienna.com>
2023-09-14 12:17:59 +0200

    memmove is cut down to size

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 12:01:58 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/14 11:56:23 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/14 12:17:13 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,38 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*un_dest;
-	unsigned char	*un_src;
 	size_t			i;
 
-	un_dest = (unsigned char *)dest;
-	un_src = (unsigned char *)src;
-	if (un_dest > un_src && un_src + n > un_dest)
+	if ((unsigned char *)dest > (unsigned char *)src
+		&& (unsigned char *)src + n > (unsigned char *)dest)
 	{
 		i = n;
 		while (i)
 		{
-			*(un_dest + i) = *(un_src + i);
+			*((unsigned char *)dest + i) = *((unsigned char *)src + i);
 			i--;
 		}
-		return ((void *)un_dest);
+		return ((void *)dest);
 	}
 	else
 	{
 		i = 0;
 		while (n > i)
 		{
-			*(un_dest + i) = *(un_src + i);
+			*((unsigned char *)dest + i) = *((unsigned char *)src + i);
 			i++;
 		}
-		return ((void *)un_dest);
+		return ((void *)dest);
 	}
 }
 
-#include <stdio.h>
-int main(void)
-{
-	unsigned char dest[50];
-	unsigned char src[] = "Hello World";
-	size_t n = 5;
+// #include <stdio.h>
+// int main(void)
+// {
+// 	unsigned char dest[50];
+// 	unsigned char src[] = "Hello World";
+// 	size_t n = 5;
 
-	ft_memmove(dest, src, n);
-	printf("%s", dest);
-}
+// 	ft_memmove(dest, src, n);
+// 	printf("%s", dest);
+// }
