b56a5b32f33befa0732d565feea091543065ee1c
David Ulrich <dulrich@c4r4p5.42vienna.com>
2023-09-18 17:04:39 +0200

    updating - split and itoa still not working correctly

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 10:44:35 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/18 13:26:13 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/18 14:24:22 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	un_dest = (unsigned char *)dest;
 	un_src = (unsigned char *)src;
 	i = 0;
-	while (un_src[i] && i < n)
+	while (i < n)
 	{
 		un_dest[i] = un_src[i];
 		i++;
--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,45 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 12:26:31 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/14 13:48:43 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/18 15:58:44 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		len = 0;
+	else if (start + len > ft_strlen(s))
+		len = ft_strlen(s) - start;
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
 
 static int	ft_count(const char *s, int c)
 {
@@ -94,55 +128,20 @@ char	**ft_split(char const *s, char c)
 	return (str_arr);
 }
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**str_arr;
-// 	int		i;
-// 	int		num_of_str;
-// 	int		substr_len;
+#include <stdio.h>
+int main(void)
+{
+	char c = ' ';
+	char *s = " Hippo po ta mus ";
+	char **result;
 
-// 	if (!s)
-// 		return (NULL);
-// 	num_of_str = ft_count(s, c);
-// 	str_arr = (char **)malloc((num_of_str + 1) * sizeof(char *));
-// 	if (!str_arr)
-// 		return (NULL);
-// 	i = 0;
-// 	while (num_of_str > 0)
-// 	{
-// 		while (*s && *s == c)
-// 			s++;
-// 		substr_len = 0;
-// 		while (s[substr_len] && s[substr_len] != c)
-// 			substr_len++;
-// 		str_arr[i] = ft_substr(s, 0, substr_len);
-// 		if (!str_arr[i])
-// 		{
-// 			ft_free_split(str_arr);
-// 			return (NULL);
-// 		}
-// 		s += substr_len;
-// 		i++;
-// 		num_of_str--;
-// 	}
-// 	str_arr[i] = NULL;
-// 	return (str_arr);
-// }
-
-// #include <stdio.h>
-// int main(void)
-// {
-// 	char c = ',';
-// 	char *s = ",,Hippo,,,,po,ta,mus,,";
-// 	char **result;
-
-// 	result = ft_split(s, c);
-// 	int i = 0;
-// 	while (result[i])
-// 	{
-// 		printf("%s", result[i]);
-// 		i++;
-// 	}
-// 	ft_free_split(result);
-// 	return (0);
-// }
+	result = ft_split(s, c);
+	int i = 0;
+	while (result[i])
+	{
+		printf("%s", result[i]);
+		i++;
+	}
+	ft_free_split(result);
+	return (0);
+}
