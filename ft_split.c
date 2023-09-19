09a5f2454754bd280c73d55a06d9b89be4a9f59b
David Ulrich <dulrich@c4r4p5.42vienna.com>
2023-09-19 13:00:50 +0200

    It's finally being accepted by francinette

--- ft_split.c
+++ ft_split.c
@@ -6,45 +6,45 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 12:26:31 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/18 15:58:44 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/19 11:28:20 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
+// 	i = 0;
+// 	while (s[i])
+// 		i++;
+// 	return (i);
+// }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*substr;
-	size_t	i;
+// char	*ft_substr(char const *s, unsigned int start, size_t len)
+// {
+// 	char	*substr;
+// 	size_t	i;
 
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		len = 0;
-	else if (start + len > ft_strlen(s))
-		len = ft_strlen(s) - start;
-	substr = (char *)malloc((len + 1) * sizeof(char));
-	if (!substr)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	substr[i] = '\0';
-	return (substr);
-}
+// 	if (!s)
+// 		return (NULL);
+// 	if (start >= ft_strlen(s))
+// 		len = 0;
+// 	else if (start + len > ft_strlen(s))
+// 		len = ft_strlen(s) - start;
+// 	substr = (char *)malloc((len + 1) * sizeof(char));
+// 	if (!substr)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		substr[i] = s[start + i];
+// 		i++;
+// 	}
+// 	substr[i] = '\0';
+// 	return (substr);
+// }
 
 static int	ft_count(const char *s, int c)
 {
@@ -88,16 +88,19 @@ static char	*ft_get_next_substr(const char **s, char c)
 {
 	const char	*start;
 	const char	*end;
+	size_t		len;
+	char		*substr;
 
 	start = *s;
+	while (*start && *start == c)
+		start++;
 	end = start;
 	while (*end && *end != c)
 		end++;
-	if (*end)
-		*s = end + 1;
-	else
-		*s = end;
-	return (ft_substr(start, 0, end - start));
+	len = end - start;
+	substr = ft_substr(start, 0, len);
+	*s = end;
+	return (substr);
 }
 
 char	**ft_split(char const *s, char c)
@@ -128,20 +131,20 @@ char	**ft_split(char const *s, char c)
 	return (str_arr);
 }
 
-#include <stdio.h>
-int main(void)
-{
-	char c = ' ';
-	char *s = " Hippo po ta mus ";
-	char **result;
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char c = ' ';
+// 	char *s = " Hippo po ta mus ";
+// 	char **result;
 
-	result = ft_split(s, c);
-	int i = 0;
-	while (result[i])
-	{
-		printf("%s", result[i]);
-		i++;
-	}
-	ft_free_split(result);
-	return (0);
-}
+// 	result = ft_split(s, c);
+// 	int i = 0;
+// 	while (result[i])
+// 	{
+// 		printf("%s", result[i]);
+// 		i++;
+// 	}
+// 	ft_free_split(result);
+// 	return (0);
+// }
